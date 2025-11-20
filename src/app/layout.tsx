71e0b081631ae5426e11fd2b770c08f801b3ef08
kaeun <pvteam@pulsewave.kr>
2025-11-20 17:23:10 +0900

    2025-11-20

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import Script from 'next/script'
 import './globals.css'
 import Header from '@/components/common/Header'
 import FooterWrapper from '@/components/common/FooterWrapper'
@@ -24,22 +23,6 @@ icons: { icon: '/favicon.ico' }
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 return (
 <html lang="ko">
-<head>
-<Script id="fb-pixel" strategy="beforeInteractive">
-{`
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '824895280150026');
-fbq('track', 'PageView');
-`}
-</Script>
-</head>
 <body className="relative flex flex-col min-h-screen bg-white text-gray-900 antialiased">
 <noscript>
 <img height="1" width="1" style={{display:'none'}} src="https://www.facebook.com/tr?id=824895280150026&ev=PageView&noscript=1" />
